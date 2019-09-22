0a9675b6c296f1e2b91a36b76e092857c56b93a6
mechanizzed <dsibr@hotmail.com>
2019-09-22 18:52:28 -0300

    [ADD] - Send mail in ForgotPasswordController

--- app/Controllers/Http/ForgotPassword/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPassword/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -10,7 +11,27 @@ class ForgotPasswordController {
       const user = await User.findByOrFail('email', email)
       user.token = crypto.randomBytes(10).toString('hex')
       user.token_created_at = new Date()
-      await user.save()
+      const update = await user.save()
+
+      if (update) {
+        await Mail.send(
+          ['emails.forgot_password'],
+          {
+            email,
+            token: user.token,
+            link_to_front: `${request.input('redirect_url')}?token=${
+              user.token
+            }`
+          },
+          message => {
+            message
+              .to(user.email)
+              .from('api@email.com', 'JWT Auth with AdonisJS')
+              .subject('Forgot password')
+          }
+        )
+      }
+
       return response.send({ message: 'Message OK' })
     } catch (error) {
       return response
